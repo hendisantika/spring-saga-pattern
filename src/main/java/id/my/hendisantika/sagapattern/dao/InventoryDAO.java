459ddeb877e74a108ac225e56eaea482890a8732
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:40:35 +0700

    Add readRestaurant in InventoryDAO🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/dao/InventoryDAO.java
+++ src/main/java/id/my/hendisantika/sagapattern/dao/InventoryDAO.java
@@ -1,5 +1,12 @@
 package id.my.hendisantika.sagapattern.dao;
 
+import id.my.hendisantika.sagapattern.dto.Restaurant;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : saga-pattern
@@ -16,4 +23,20 @@ public class InventoryDAO extends BaseDAO {
         super(url);
     }
 
+    public void readRestaurant(int restaurantId, Restaurant restaurant) {
+        String sql = "SELECT name, address, contact FROM restaurants WHERE id = ?";
+
+        try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, restaurantId);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                restaurant.setName(rs.getString("name"));
+                restaurant.setAddress(rs.getString("address"));
+                restaurant.setContact(rs.getString("contact"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
