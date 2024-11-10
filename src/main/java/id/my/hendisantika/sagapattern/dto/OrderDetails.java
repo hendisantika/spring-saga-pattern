89ab5960abb372cdaea95c2eb64592797beb59aa
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:33:51 +0700

    Add OrderDetails DTO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dto/OrderDetails.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.sagapattern.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class OrderDetails {
+    private String orderId;
+    private ArrayList<FoodItem> items;
+    private ArrayList<String> notes;
+}
