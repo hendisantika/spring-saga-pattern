bc03c08cf3d005b6e50f70bb20e12654705239a9
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:36:56 +0700

    Add BaseDAO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dao/BaseDAO.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.sagapattern.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.36
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseDAO {
+    private final String url;
+
+    public BaseDAO(String url) {
+        this.url = url;
+    }
+}
