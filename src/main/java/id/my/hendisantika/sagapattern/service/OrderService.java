60ee8e451c68a5c250ef2c14166c7b94d9264f8d
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:55:22 +0700

    Add createOrder in OrderService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/service/OrderService.java
+++ src/main/java/id/my/hendisantika/sagapattern/service/OrderService.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.sagapattern.service;
 
 import id.my.hendisantika.sagapattern.dao.OrdersDAO;
+import id.my.hendisantika.sagapattern.dto.Customer;
+import id.my.hendisantika.sagapattern.dto.Order;
+import id.my.hendisantika.sagapattern.dto.OrderDetails;
+import id.my.hendisantika.sagapattern.dto.OrderRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,43 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private static final OrdersDAO ORDERS_DAO = new OrdersDAO("jdbc:sqlite:food_delivery.db");
+
+    public static String createOrder(OrderRequest orderRequest) {
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+
+        Order order = new Order();
+        order.setOrderId(uuidAsString);
+
+        Customer customer = new Customer();
+        customer.setEmail(orderRequest.getCustomerEmail());
+        customer.setName(orderRequest.getCustomerName());
+        customer.setContact(orderRequest.getCustomerContact());
+        customer.setId(ORDERS_DAO.insertCustomer(customer));
+
+        log.info("Upsert customer record in DB with id: {}", customer.getId());
+
+        order.setCustomer(customer);
+        order.setRestaurantId(orderRequest.getRestaurantId());
+        order.setDeliveryAddress(orderRequest.getDeliveryAddress());
+        order.setStatus(Order.Status.PENDING);
+
+        OrderDetails orderDetails = new OrderDetails();
+        orderDetails.setOrderId(uuidAsString);
+        orderDetails.setItems(orderRequest.getItems());
+        orderDetails.setNotes(orderRequest.getNotes());
+
+        order.setOrderDetails(orderDetails);
+
+        String error = ORDERS_DAO.insertOrder(order);
+
+        if (error.isEmpty()) {
+            log.info("Created order with id: {}", order.getOrderId());
+        } else {
+            log.error("Order creation failure: {}", error);
+            return null;
+        }
+
+        return uuidAsString;
+    }
 }
