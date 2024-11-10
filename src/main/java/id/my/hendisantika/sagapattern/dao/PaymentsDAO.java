e6af98d6f4729a9ff431dbe1447f5dcf94878a79
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:43:17 +0700

    Add insertPayment in PaymentsDAO🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/dao/PaymentsDAO.java
+++ src/main/java/id/my/hendisantika/sagapattern/dao/PaymentsDAO.java
@@ -1,5 +1,13 @@
 package id.my.hendisantika.sagapattern.dao;
 
+import id.my.hendisantika.sagapattern.dto.Payment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : saga-pattern
@@ -14,5 +22,26 @@ public class PaymentsDAO extends BaseDAO {
 
     public PaymentsDAO(String url) {
         super(url);
+    }
+
+    public String insertPayment(Payment payment) {
+        Date date = new Date();
+        Timestamp nowAsTS = new Timestamp(date.getTime());
+
+        String sql = "INSERT INTO payments(paymentid, orderid, amount, method, createdat, status) VALUES(?,?,?,?,?,?);";
+
+        try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, payment.getPaymentId());
+            pstmt.setString(2, payment.getOrderId());
+            pstmt.setDouble(3, payment.getAmount());
+            pstmt.setString(4, payment.getPaymentMethod().toString());
+            pstmt.setTimestamp(5, nowAsTS);
+            pstmt.setString(6, payment.getStatus().name());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+        return "";
     }
 }
