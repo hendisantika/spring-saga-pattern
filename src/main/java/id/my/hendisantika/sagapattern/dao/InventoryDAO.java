c674914754c12542d68638561c6ebf24646a7068
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:40:23 +0700

    Add InventoryDAO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dao/InventoryDAO.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.sagapattern.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.40
+ * To change this template use File | Settings | File Templates.
+ */
+public class InventoryDAO extends BaseDAO {
+
+    public InventoryDAO(String url) {
+        super(url);
+    }
+
+}
