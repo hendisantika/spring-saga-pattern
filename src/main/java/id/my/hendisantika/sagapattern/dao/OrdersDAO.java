e8b4a4925160d08160a0ea72286ea39c447f9771
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:40:57 +0700

    Add OrdersDAO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dao/OrdersDAO.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.sagapattern.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.40
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrdersDAO extends BaseDAO {
+
+    public OrdersDAO(String url) {
+        super(url);
+    }
+}
