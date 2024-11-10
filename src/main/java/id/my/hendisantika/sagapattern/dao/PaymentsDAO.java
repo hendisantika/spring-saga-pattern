6aa0779ed1e10d9d091b82589b7c81e24220af8a
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:42:35 +0700

    Add PaymentsDAO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dao/PaymentsDAO.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.sagapattern.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.42
+ * To change this template use File | Settings | File Templates.
+ */
+public class PaymentsDAO extends BaseDAO {
+
+    public PaymentsDAO(String url) {
+        super(url);
+    }
+}
