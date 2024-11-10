02388310db55d956ac95e169bcde0105e1e12634
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:31:50 +0700

    Add Customer DTO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dto/Customer.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.sagapattern.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class Customer {
+    private int id;
+    private String email;
+    private String name;
+    private String contact;
+}
