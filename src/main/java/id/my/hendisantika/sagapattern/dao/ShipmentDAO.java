bde2d9ac0c4961fa9350817cf779ea5709244a60
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:44:22 +0700

    Add insertShipment in ShipmentDAO🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/dao/ShipmentDAO.java
+++ src/main/java/id/my/hendisantika/sagapattern/dao/ShipmentDAO.java
@@ -1,5 +1,13 @@
 package id.my.hendisantika.sagapattern.dao;
 
+import id.my.hendisantika.sagapattern.dto.Shipment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : saga-pattern
@@ -14,5 +22,26 @@ public class ShipmentDAO extends BaseDAO {
 
     public ShipmentDAO(String url) {
         super(url);
+    }
+
+    public boolean insertShipment(Shipment shipment) {
+        Date date = new Date();
+        Timestamp nowAsTS = new Timestamp(date.getTime());
+
+        String sql = "INSERT INTO shipments(orderid,driverid,address,instructions,createdat,status) VALUES(?,?,?,?,?,?)";
+
+        try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, shipment.getOrderId());
+            pstmt.setInt(2, shipment.getDriverId());
+            pstmt.setString(3, shipment.getDeliveryAddress());
+            pstmt.setString(4, shipment.getDeliveryInstructions());
+            pstmt.setTimestamp(5, nowAsTS);
+            pstmt.setString(6, Shipment.Status.SCHEDULED.name());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
