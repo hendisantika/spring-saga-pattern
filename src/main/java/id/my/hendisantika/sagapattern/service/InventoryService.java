d0b5f1ea92d59837d8ba99fcfe96ca56f471972b
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:54:42 +0700

    Add checkAvailability in InventoryService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/service/InventoryService.java
+++ src/main/java/id/my/hendisantika/sagapattern/service/InventoryService.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.sagapattern.service;
 
 import id.my.hendisantika.sagapattern.dao.InventoryDAO;
+import id.my.hendisantika.sagapattern.dto.FoodItem;
+import id.my.hendisantika.sagapattern.dto.Restaurant;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +22,12 @@ import lombok.extern.slf4j.Slf4j;
 public class InventoryService {
 
     private static final InventoryDAO inventoryDAO = new InventoryDAO("jdbc:sqlite:food_delivery.db");
+
+    public static boolean checkAvailability(int restaurantId, ArrayList<FoodItem> items) {
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(restaurantId);
+        restaurant.setName("");
+        inventoryDAO.readRestaurant(restaurantId, restaurant);
+        return !Objects.equals(restaurant.getName(), "");
+    }
 }
