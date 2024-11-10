ea80736219df5bfef1b75364203d74cbd3f6ffd6
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:44:04 +0700

    Add ShipmentDAO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dao/ShipmentDAO.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.sagapattern.dao;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.43
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShipmentDAO extends BaseDAO {
+
+    public ShipmentDAO(String url) {
+        super(url);
+    }
+}
