02d84442ad88a77b5beba3ff952960f02e6ebdc9
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 06:35:43 +0700

    Add Restaurant DTO🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/dto/Restaurant.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.sagapattern.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 06.35
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class Restaurant {
+    int id;
+    String name;
+    String address;
+    String contact;
+}
