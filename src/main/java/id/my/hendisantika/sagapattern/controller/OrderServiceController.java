d7310f4b915575e1cea71a0acfe86e63cd661c99
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 09:16:51 +0700

    Add triggerFoodDeliveryFlow in OrderServiceController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/controller/OrderServiceController.java
+++ src/main/java/id/my/hendisantika/sagapattern/controller/OrderServiceController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.sagapattern.controller;
 
+import id.my.hendisantika.sagapattern.dto.FoodDeliveryRequest;
 import id.my.hendisantika.sagapattern.service.WorkflowService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderServiceController {
 
     private final WorkflowService workflowService;
+
+    @PostMapping(value = "/triggerFoodDeliveryFlow", produces = "application/json")
+    public ResponseEntity<Map<String, Object>> triggerFoodDeliveryFlow(@RequestBody FoodDeliveryRequest foodDeliveryRequest) {
+        return ResponseEntity.ok(workflowService.startFoodDeliveryWorkflow(foodDeliveryRequest));
+    }
 }
