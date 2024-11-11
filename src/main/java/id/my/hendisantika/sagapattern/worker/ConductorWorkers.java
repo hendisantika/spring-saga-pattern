cf70349833a77f373122b37f9082c24d2f04df7b
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 07:07:00 +0700

    Add orderFoodTask in ConductorWorkers🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/worker/ConductorWorkers.java
+++ src/main/java/id/my/hendisantika/sagapattern/worker/ConductorWorkers.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.sagapattern.worker;
 
+import id.my.hendisantika.sagapattern.dto.OrderRequest;
+import id.my.hendisantika.sagapattern.service.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,24 @@ import org.springframework.stereotype.Component;
 @Component
 @ComponentScan(basePackages = {"io.orkes"})
 public class ConductorWorkers {
+    /**
+     * Note: Using this setting, up to 5 tasks will run in parallel, with tasks being polled every 200ms
+     */
+    @WorkerTask(value = "order_food", threadCount = 3, pollingInterval = 300)
+    public TaskResult orderFoodTask(OrderRequest orderRequest) {
+        String orderId = OrderService.createOrder(orderRequest);
+
+        TaskResult result = new TaskResult();
+        Map<String, Object> output = new HashMap<>();
+
+        if (orderId != null) {
+            output.put("orderId", orderId);
+            result.setOutputData(output);
+            result.setStatus(TaskResult.Status.COMPLETED);
+        } else {
+            output.put("orderId", null);
+            result.setStatus(TaskResult.Status.FAILED);
+        }
+        return result;
+    }
 }
