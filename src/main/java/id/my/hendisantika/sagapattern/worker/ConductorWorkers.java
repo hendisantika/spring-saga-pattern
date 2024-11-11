4ccbfe3554f080b7c0c2a208ada56824526d35da
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 07:06:49 +0700

    Add ConductorWorkers🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/sagapattern/worker/ConductorWorkers.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.sagapattern.worker;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/24
+ * Time: 07.06
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+@Component
+@ComponentScan(basePackages = {"io.orkes"})
+public class ConductorWorkers {
+}
