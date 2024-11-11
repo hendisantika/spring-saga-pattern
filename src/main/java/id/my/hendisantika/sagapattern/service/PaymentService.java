b711532b6bc529e65399211186e68309194d1005
hendisantika <hendisantika@yahoo.co.id>
2024-11-11 07:03:17 +0700

    Add makePayment in PaymentService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/sagapattern/service/PaymentService.java
+++ src/main/java/id/my/hendisantika/sagapattern/service/PaymentService.java
@@ -2,9 +2,15 @@ package id.my.hendisantika.sagapattern.service;
 
 import id.my.hendisantika.sagapattern.dao.PaymentsDAO;
 import id.my.hendisantika.sagapattern.dto.Payment;
+import id.my.hendisantika.sagapattern.dto.PaymentDetails;
 import id.my.hendisantika.sagapattern.dto.PaymentRequest;
 import lombok.extern.slf4j.Slf4j;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -57,5 +63,31 @@ public class PaymentService {
         paymentsDAO.readPayment(orderId, payment);
         payment.setStatus(Payment.Status.CANCELED);
         paymentsDAO.updatePaymentStatus(payment);
+    }
+
+    private static boolean makePayment(Payment payment) {
+        if (Objects.equals(payment.getPaymentMethod().getType(), "Credit Card")) {
+            PaymentDetails details = payment.getPaymentMethod().getDetails();
+
+            DateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
+            Date expiry = new Date();
+
+            try {
+                expiry = dateFormat.parse(details.getExpiry());
+            } catch (ParseException e) {
+                payment.setErrorMsg("Invalid expiry date:" + details.getExpiry());
+                return false;
+            }
+
+            Date today = new Date();
+            if (today.getTime() > expiry.getTime()) {
+                payment.setErrorMsg("Expired payment method:" + details.getExpiry());
+                return false;
+            }
+
+        }
+        // Ideally an async call would be made with a callback
+        // But, we're skipping that and assuming payment went through
+        return true;
     }
 }
